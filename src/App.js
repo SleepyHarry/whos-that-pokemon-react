0becb0ef17b35cf47db8217acac24c20ee16af78
SleepyHarry <harryavane@gmail.com>
2017-05-29 00:17:14 +0100

    Add basic guessing mechanic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React, {Component} from 'react';
-import {Button} from 'react-bootstrap';
-import './App.css';
+import React, {Component} from "react";
+import {Button, Col, FormControl, FormGroup} from "react-bootstrap";
+import "./App.css";
 
-import names from './names.json';
+import names from "./names.json";
 import Pokemon from "./Pokemon";
 
 class App extends Component {
-    randPoke() {
-        // TODO: Generation filtering
-        return names[Math.floor(Math.random() * 151)];
-    }
-
     constructor(props) {
         super(props);
 
         this.state = {
             pokemon: this.randPoke(),
             hidden: false,
+
+            currentGuess: '',
         };
     }
 
@@ -32,15 +29,57 @@ class App extends Component {
         });
     }
 
+    randPoke() {
+        // TODO: Generation filtering
+        return names[Math.floor(Math.random() * 151)];
+    }
+
+    onChange(e) {
+        this.setState({
+            currentGuess: e.target.value,
+        });
+    }
+
+    onSubmit() {
+        if (this.state.currentGuess.toLowerCase() === this.state.pokemon.name) {
+            alert('correct!');
+            this.setState({
+                pokemon: this.randPoke(),
+                currentGuess: '',
+            });
+        } else {
+            alert('nope!');
+        }
+    }
+
     render() {
         return (
             <div>
-                <Button onClick={this.newPoke.bind(this)}>
-                    shuffle
-                </Button>
-                <Button onClick={this.toggleHidden.bind(this)}>
-                    {this.state.hidden ? 'reveal' : 'hide'}
-                </Button>
+                <Col xs={6}>
+                    <Button onClick={this.newPoke.bind(this)}>
+                        Shuffle
+                    </Button>
+                    <Button onClick={this.toggleHidden.bind(this)}>
+                        {this.state.hidden ? 'Reveal' : 'Hide'}
+                    </Button>
+                </Col>
+                <Col xs={4}>
+                    <FormGroup validationState={null}>
+                        <FormControl
+                            type="text"
+                            placeholder="Your guess"
+                            onChange={this.onChange.bind(this)}
+                        />
+                    </FormGroup>
+                </Col>
+                <Col xs={2}>
+                    <Button
+                        type="submit"
+                        onClick={this.onSubmit.bind(this)}
+                    >
+                        Guess
+                    </Button>
+                </Col>
                 <Pokemon
                     {...this.state.pokemon}
                     hidden={this.state.hidden}
