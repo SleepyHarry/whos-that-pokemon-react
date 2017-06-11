beb6f1bdfe5bf99c9e783967543e561ad6a2294f
SleepyHarry <harryavane@gmail.com>
2017-06-11 01:45:02 +0100

    Add levenshtein difference calculator

---
+++ src/levenshtein.js
@@ -0,0 +1,53 @@
+/*
+ * Calculate Levenshtein distance between two strings
+ * based on https://people.cs.pitt.edu/~kirk/cs1501/Pruhs/Spring2006/assignments/editdistance/Levenshtein%20Distance.htm
+ */
+
+
+// thx https://stackoverflow.com/a/39243778/5022115
+const matrix = (m, n) => {
+    let result = [];
+    for(let i = 0; i < n; i++) {
+        result.push(new Array(m).fill(0));
+    }
+    return result;
+};
+
+
+export default levenshtein = (s, t) => {
+    s = s.toLowerCase();
+    t = t.toLowerCase();
+
+    const n = s.length;
+    const m = t.length;
+
+    if (n === 0 || m === 0) {
+        return n + m;
+    }
+
+    const mat = matrix(m + 1, n + 1);
+
+    for (let i = 0; i <= n; i++) {
+        mat[i][0] = i;
+    }
+
+    for (let j = 0; j <= m; j++) {
+        mat[0][j] = j;
+    }
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            const cost = !(s[i] === t[j]);  // true has value 1 when added, false 0
+
+            mat[i + 1][j + 1] = Math.min(
+                mat[i][j + 1] + 1,
+                mat[i + 1][j] + 1,
+                mat[i][j] + cost,
+            );
+        }
+    }
+
+    console.log(mat);
+
+    return mat[n][m];
+};
