193b3428c88c551bbd14b13247e6b1712ff31e99
SleepyHarry <harryavane@gmail.com>
2017-07-10 22:18:10 +0100

    Setting up Flask backend properly

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder='build', static_url_path='',
+    template_folder='build',
+)
 
 
 ENV = os.environ.get('ENV', 'Dev')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
